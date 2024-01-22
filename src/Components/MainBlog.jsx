5c6a4ed9484ffd7cfb3c30a539f23e203a072d09
1Ajay1 <ajayverma2028@gmail.com>
2024-01-22 17:03:29 +0530

    Added message

--- src/Components/MainBlog.jsx
+++ src/Components/MainBlog.jsx
@@ -15,7 +15,7 @@ const MainBlog = () => {
                         <p className="blog-post-meta">January 1, 2021 by <a href="#">Mark</a></p>
 
                         <p>This blog post shows a few different types of content that’s supported and styled with Bootstrap. Basic
-                            typography, lists, tables, images, code, and more are all supported as expected.</p>
+                            typography, lists, tables, images, code, and more are all supported as expected is best test.</p>
                         <hr />
                         <p>This is some additional paragraph placeholder content. It has been written to fill the available space
                             and show how a longer snippet of text affects the surrounding content. We'll repeat it often to keep the
